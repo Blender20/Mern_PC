16621038a51a437e2109a42057f70a9d30f06f7d
Blender20 <rayyazdi@outlook.com>
2022-05-08 23:14:44 -0400

    More Forms

--- 03_hook_form/src/components/Form.jsx
+++ 03_hook_form/src/components/Form.jsx
@@ -4,15 +4,29 @@ const Form = ()=>{
 
     // Create a State variable for each input that will store the information inside each input.
     let [firstName, setName] = useState("");
-    let [lastName, setlastName] = useState("");
+    let [lastName, setLastName] = useState("");
     let [age, setAge] = useState("");
     let [email, setEmail] = useState("");
     let [password, setPassword] = useState("");
-    let [ConfirmPassword, setConfPassword] = useState("");
+    let [confirmPassword, setConfPassword] = useState("");
     let [type, setType] = useState("");
     let [date, setDate] = useState("");
     let [description, setDescription] = useState("");
     let [imgUrl, setImgUrl] = useState("");
+    
+    let currentDate = new Date();
+    let formDate = new Date(date);
+    
+    
+    
+    
+    // const showNameError = ()=>{ one way of writing a if statement!!!
+    //     if(firstName.length<3){
+    //         return <p>First Name must be at least three characters!</p>
+    //     }else{
+    //         return null;
+    //     }
+    // }
     
     return(
         <>
@@ -21,26 +35,45 @@ const Form = ()=>{
                 <div className="form-group">
                     <label htmlFor="">First Name:</label>
                     <input type="text" onChange={(e)=>setName(e.target.value)} name="" id="" className="form-control" />
+                    {
+                        // showNameError()
+                        firstName.length<3? <p className='text-success'>First Name must be at least three characters!</p> : null
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Last Name:</label>
-                    <input type="text" onChange={(e)=>setlastName(e.target.value)} name="" id="" className="form-control" />
+                    <input type="text" onChange={(e)=>setLastName(e.target.value)} name="" id="" className="form-control" />
+                    {
+                    lastName.length<3? <p className='text-success'>Last Name must be at least three characters!</p> : null
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Age:</label>
                     <input type="number" onChange={(e)=>setAge(e.target.value)} name="" id="" className="form-control" />
-                </div> */}
+                    {
+                    age<1? <p className='text-success'>Please enter a valid age!</p> : null
+                    }
+                </div> 
                 <div className="form-group">
                     <label htmlFor="">Email:</label>
                     <input type="text" onChange={(e)=>setEmail(e.target.value)} name="" id="" className="form-control" />
+                    {
+                    email.length<5? <p className='text-success'>Please enter a valid email!</p> : null
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Password:</label>
                     <input type="text" onChange={(e)=>setPassword(e.target.value)} name="" id="" className="form-control" />
+                    {
+                    password.length<8? <p className='text-success'>Password must be 8 characters long!</p> : null
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Confirm Password:</label>
                     <input type="text" onChange={(e)=>setConfPassword(e.target.value)} name="" id="" className="form-control" />
+                    {
+                    confirmPassword != password? <p className='text-success'>Password must be 8 characters long!</p> : null
+                    }
                 </div>
                 {/* <div className="form-group">
                     <label htmlFor="">Type:</label>
@@ -53,6 +86,9 @@ const Form = ()=>{
                 {/* <div className="form-group">
                     <label htmlFor="">Appointment Date:</label>
                     <input type="date" onChange={(e)=>setDate(e.target.value)} name="" id="" className="form-control" />
+                    {
+                    <formDate<currentDate? <p className='text-success'>Please enter a valid current date!</p> : null
+                    }
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Description:</label>
@@ -72,7 +108,7 @@ const Form = ()=>{
             {/* <p>Age: {age}</p> */}
             <p>Email: {email}</p>
             <p>Password: {password}</p>
-            <p>Confirm Password: {ConfirmPassword}</p>
+            <p>Confirm Password: {confirmPassword}</p>
             {/* <p>Type: {type}</p> */}
             {/* <p>Date: {date}</p> */}
             {/* <p>Description: {description}</p> */}
